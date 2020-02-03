8e8a7ba87ecdcd57cdc2c2f5b527c791af22846d
NiamhAndrews <neand2006@gmail.net>
2020-02-03 17:00:18 +0000

    Tried to slow Neutral piece movement

--- LGame/Neutral.cpp
+++ LGame/Neutral.cpp
@@ -11,23 +11,28 @@ Neutral::Neutral()
 
 void Neutral::init()
 {
-	neutral1.setRadius(35);
+	neutral1.setRadius(40);
 	neutral1.setFillColor(sf::Color::Yellow);
-	neutral1.setPosition(225, 225);
-	neutral2.setRadius(35);
+	neutral1.setPosition(208, 208);
+	neutral2.setRadius(40);
 	neutral2.setFillColor(sf::Color::Yellow);
-	neutral2.setPosition(500, 500);
+	neutral2.setPosition(508, 308);
 }
 
 void Neutral::move()
 {
+	
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		moveLeft1Square();
+		if (sf::Event::KeyReleased)//----------------------------- remove if not helping stop movement after 1 press
+		{
+			moveLeft1Square();
+
+		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		moveRight1Square();
+			moveRight1Square();
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
@@ -42,68 +47,60 @@ void Neutral::move()
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
-		neutral2.move(-3, 0);
+		moveN2Left1Square();
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
-		neutral2.move(3, 0);
+		moveN2Right1Square();
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
-		neutral2.move(0, -3);
+		moveN2Up1Square();
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 	{
-		neutral2.move(0, 3);
+		moveN2Down1Square();
 	}
+	 
 }
+
+
+
 
 void Neutral::moveLeft1Square()
 {
-
 	neutral1.move(-100, 0);
 }
 void Neutral::moveRight1Square()
 {
-
 	neutral1.move(100, 0);
 }
 void Neutral::moveUp1Square()
 {
-
 	neutral1.move(0, -100);
 }
 void Neutral::moveDown1Square()
 {
-
 	neutral1.move(0, 100);
 }
 
 
 void Neutral::moveN2Left1Square()
 {
-
 	neutral2.move(-100, 0);
 }
 void Neutral::moveN2Right1Square()
 {
-
 	neutral2.move(100, 0);
 }
 void Neutral::moveN2Up1Square()
 {
-
 	neutral2.move(0, -100);
 }
 void Neutral::moveN2Down1Square()
 {
-
 	neutral2.move(0, 100);
 }
-
-
-
-
 
 
 
